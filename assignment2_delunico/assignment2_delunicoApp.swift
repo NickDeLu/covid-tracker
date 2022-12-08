a27b50419a78c536136eed4be00dd55680ea40ec
Nick De Luca <nicolasdeluca_8@hotmail.com>
2022-12-07 20:16:35 -0500

    Initial Commit

---
+++ assignment2_delunico/assignment2_delunicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  assignment2_delunicoApp.swift
+//  assignment2_delunico
+//
+//  Created by Nick De Luca on 2022-12-07.
+//
+
+import SwiftUI
+
+@main
+struct assignment2_delunicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
